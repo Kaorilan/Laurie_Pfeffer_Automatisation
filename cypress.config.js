af8dd10a6a2a6252acb97e42fe8deccde2804cf5
Laurie Pfeffer <pfefferlaurie@gmail.com>
2025-12-18 16:31:40 +0100

    config cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,10 @@
 const { defineConfig } = require('cypress');
-const fetch = require('node-fetch');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200',
-    setupNodeEvents(on, config) {
-      on('task', {
-        async checkServer() {
-          try {
-            const res = await fetch('http://localhost:4200');
-            return res.ok ? null : 'Server not running on 4200';
-          } catch {
-            return 'Server not running on 4200 – lancez npm start';
-          }
-        }
-      });
-    },
+    baseUrl: 'http://localhost:4200',  // Front Angular
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
   },
 });
